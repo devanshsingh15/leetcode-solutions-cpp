cef6b2e6d9fd7c2eca82a42694c45bb3a15f1809
Devansh <157308124+devanshsingh15@users.noreply.github.com>
2024-01-30 23:50:06 +0530

    Create 7. Reverse Integer.cpp

---
+++ 7. Reverse Integer.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int reverse(int x) {
+        int reverse=0;
+        while(x!=0){
+            int digit=x%10;
+            if((reverse>INT_MAX/10) || (reverse<INT_MIN/10)) {
+                return 0;
+            }
+            reverse=reverse*10+digit;
+            x=x/10;
+        }
+        return reverse;
+    }
+};
