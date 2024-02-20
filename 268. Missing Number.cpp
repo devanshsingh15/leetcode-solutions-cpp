62a712552db9aa6706ecaf88bcaab9692b54b01a
Devansh <157308124+devanshsingh15@users.noreply.github.com>
2024-02-20 09:50:15 +0530

    Create 268. Missing Number.cpp

---
+++ 268. Missing Number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n=nums.size();
+        int a=n*(n+1)/2;
+        int sum=0;
+        for(int i=0;i<n;i++)
+        {
+            sum=sum+nums[i];
+        }
+        return (a-sum);
+    }
+};
